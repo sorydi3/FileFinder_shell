128c8a808a16eee724dd8471448f2faeeb59fa3f
Diallo Ibrahima <ibrahimasory22@hotmail.com>
2018-04-08 12:44:48 +0200

    fixed to save output in another file

--- busca.sh
+++ busca.sh
@@ -1,4 +1,5 @@
- #rmdir ~/resultat && mkdir ~/resultat
+
+#seleciona els variables  passat per parametres
 while getopts n:i:p:o:d:s: option
 do
 case "${option}"
@@ -12,14 +13,14 @@ s) GUARDA=$OPTARG;;
 ?) WRONG=$OPTARG;;
 esac
 done
-
+#funcio recursiva
 recurse_path () {
    path=`pwd`
-            if [ -z  $FITXER ]
+            if [ -z  $GUARDA ]
         then
-            ls -lisa |  grep "$PERMISSOS" |  grep -E ^$NUMINODE  |  grep "$FITXER$" &&  echo"torbat a $path"
+            ls -lisa |  grep "$PERMISSOS" |  grep -E ^$NUMINODE  |  grep "$FITXER$" &&  echo "torbat a $path"
         else
-             ls -lisa |  grep "$PERMISSOS" |  grep -E ^$NUMINODE  |  grep  "$FITXER$"  &&  echo"torbat a $path"
+             (ls -lisa |  grep "$PERMISSOS" |  grep -E ^$NUMINODE  |  grep  "$FITXER$" >> $GUARDA  &&  echo"torbat a $path")2>>~/errors
      fi
     for i in $*; do
         if [ -d "${i}" ] 
@@ -29,11 +30,12 @@ recurse_path () {
         fi
     done
 }
+#obre el directory on s executar l'escrip
 if [ ! -z  $DIRECTORY ]
         then
            cd $DIRECTORY 
      fi
-
+#crida de la funcio recursica
 recurse_path $(ls  ".")
 echo $var
 
